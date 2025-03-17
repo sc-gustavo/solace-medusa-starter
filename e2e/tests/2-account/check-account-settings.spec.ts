b436cb0c7f0041fcfd5f0557668c12a22d7b650a
M-Kaczmar <marcin.kaczmarczyk@rigbyjs.com>
2025-03-17 09:50:13 +0100

    E2E: Add flow for `check-account-settings` test; fix minor errors (WIP)

--- e2e/tests/2-account/check-account-settings.spec.ts
+++ e2e/tests/2-account/check-account-settings.spec.ts
@@ -1,19 +1,22 @@
 export{}
 
 import { test } from '@playwright/test';
+import AccountSettings from '../../fixtures/page-objects/2-account/check-account-settings';
 import helpers from '../../utils/tests-helpers';
 
-test.describe('Acount settings page', () => {
+test.describe('Account settings page', () => {
 
+  let accountSettings: AccountSettings
 
   test.beforeEach(async ({ page, browser }) => {
 
+    accountSettings = new AccountSettings(page)
 
     await helpers.waitForPageLoad(page)
   });
 
   test.afterEach(async ({ page }, testInfo) => {
-    if (testInfo.status !== 'timedOut' && testInfo.status !== 'interrupted') {
+  if (testInfo.status !== 'timedOut' && testInfo.status !== 'interrupted') {
       await page.close();
     }
   });
@@ -26,7 +29,17 @@ test.describe('Acount settings page', () => {
     // go to account
     await helpers.goToAccount(page)
 
-    
+    // go to account settings
+    await accountSettings.goToAccountSettings()
+
+    // edit acconut details
+    await accountSettings.editAccountDetails()
+
+    // check new account details
+    await accountSettings.checkNewAccountDetails()
+
+    // reverse default account details
+    await helpers.fillProfileDetails(page)
 
 })
 })
--- e2e/utils/tests-helpers.ts
+++ e2e/utils/tests-helpers.ts
@@ -163,7 +163,26 @@ export async function addNewShippingDetailsAddress(page) {
     await waitForPageLoad(page)
   }
 
+export async function fillProfileDetails(page) {
 
+    await waitForPageLoad(page)
+
+    await this.page.getByTestId('edit-details-button').nth(0).click()
+
+    await this.page.getByTestId('first-name-input').fill('');
+
+    await this.page.getByTestId('first-name-input').fill('Adam');
+  
+    await this.page.getByTestId('last-name-input').fill('');
+
+    await this.page.getByTestId('last-name-input').fill('Nowak');
+
+    await this.page.getByTestId('phone-input').fill('');
+
+    await this.page.getByTestId('phone-input').fill('99900888');
+
+    await this.page.getByTestId('save-details-button').click()
+}
 
 
 const helpers = {
@@ -178,7 +197,8 @@ const helpers = {
     fillShippingAddressInputs,
     orderProductFlow,
     goToAccount,
-    addNewShippingDetailsAddress
+    addNewShippingDetailsAddress,
+    fillProfileDetails
 };
 export default helpers;
 
