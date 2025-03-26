8fe4e23a388f9417044a3cdac1701a4e699d6274
M-Kaczmar <marcin.kaczmarczyk@rigbyjs.com>
2025-03-26 09:42:37 +0100

    E2E: Remove unecessary code

--- e2e/tests/2-account/adding-to-cart.spec.ts
+++
@@ -1,8 +0,0 @@
-await page.waitForSelector('[data-testid="delete-button"]', {
-  state: 'visible',
-})
-await page.getByTestId('delete-button').click()
-
-await expect(page).toHaveURL(/.*\/checkout\?step=address$/)
-
-const toast = await page.getByText('Product was removed from cart.')
