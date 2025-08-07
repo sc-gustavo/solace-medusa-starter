c3d9e635644f989556acd491b9573c6cfda497ea
sc-gustavo <gustavo.duran@servicioscreativos.mx>
2025-08-06 22:15:25 -0600

    feat: Add support for public Strapi domain in image configuration
    - Allow images from alienware-alpha.ddns.net:1337
    - Enable remote image loading from public Strapi instance

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
         hostname: 'localhost',
       },
       {
+        protocol: 'http',
+        hostname: 'alienware-alpha.ddns.net',
+        port: '1337',
+      },
+      {
         protocol: 'https',
         hostname: 'medusa-public-images.s3.eu-west-1.amazonaws.com',
       },
